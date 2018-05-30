192f96dd418796ce27d4fdb3b30468c3210e47a6
qqbuby <qqbuby@gmail.com>
2018-05-30 10:50:53 +0800

    Add P-CODE into BUBBLE_SORT.go

--- BUBBLE_SORT.go
+++ BUBBLE_SORT.go
@@ -14,6 +14,12 @@ func main() {
 	fmt.Printf("FIN: %v\n", A)
 }
 
+// BUBBLE_SORT(A)
+//
+// for i = 1 to A.length
+//     for j = A.length downto i + 1
+//        if A[j] < A[j-1]
+//             exchange A[j] with A[j-1]
 func BUBBLE_SORT(A []int) {
 	for i := 0; i < len(A); i++ {
 		for j := len(A) - 1; j > i; j-- {
