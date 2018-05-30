164e85f80c0e4e2925e8b9bbc9a3ade0ecd6ebf0
qqbuby <qqbuby@gmail.com>
2018-05-30 10:33:14 +0800

    Add P-CODE into MERGE_SORT.go

--- MERGE_SORT.go
+++ MERGE_SORT.go
@@ -14,6 +14,12 @@ func main() {
 	fmt.Printf("FIN: %v\n", A)
 }
 
+// MERGER_SORT(A,p,r)
+// if p < r
+//     q = [(p+r)/2] // round down
+//     MERGE_SORT(A,p,q)
+//     MERGE_SORT(A,q+1,r)
+//     MERGE(A,p,r)
 func MERGE_SORT(A []int, p, r int) {
 	if p < r {
 		q := (p + r) / 2
@@ -23,6 +29,25 @@ func MERGE_SORT(A []int, p, r int) {
 	}
 }
 
+// MERGE(A,p,q,r)
+// n1 = q - p + 1
+// n2 = r - q
+// let L[1...n1+1] and R[1..n2+1] be new arrays
+// for i = 1 to n1
+//     L[i] = A[q+i]
+// for j = 1 to n2
+//     R[j] = A[q+j+1]
+// L[n1+1] = L[n1] + R[n2]
+// R[n2+1] = L[n1] + R[n2]
+// i = 1
+// j = 1
+// for k = p to r
+//     if L[i] <= R[j]
+//         A[k] = L[i]
+//         i = i + 1
+//     else
+//         A[k] = R[j]
+//         j = j + 1
 func MERGE(A []int, p, q, r int) {
 	N1 := q - p + 1
 	N2 := r - q
