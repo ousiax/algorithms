6e4a346b39638ee860df2bc6cf1e86aa4584f73a
qqbuby <qqbuby@gmail.com>
2018-05-30 10:24:35 +0800

    Update INSERTION_SORT.go

--- INSERTION_SORT.go
+++ INSERTION_SORT.go
@@ -1,13 +1,3 @@
-// INSERT-SORT(A)
-//
-// for j = 2 to A.length
-//     key = A[j]
-//     // Insert A[j] into the sorted sequence A[1..j-1].
-//     i = j - 1
-//     while i > 0 and A[i] > key
-//         A[i+1] = A[i]
-//         i = i - 1
-//     A[i+1] = key
 package main
 
 import (
@@ -24,6 +14,16 @@ func main() {
 	fmt.Printf("FIN: %v\n", A)
 }
 
+// INSERT-SORT(A)
+//
+// for j = 2 to A.length
+//     key = A[j]
+//     // Insert A[j] into the sorted sequence A[1..j-1].
+//     i = j - 1
+//     while i > 0 and A[i] > key
+//         A[i+1] = A[i]
+//         i = i - 1
+//     A[i+1] = key
 func INSERTION_SORT(A []int) {
 	j := 1
 	for j < len(A) {
