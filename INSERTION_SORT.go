5ed6cac1b1832203d7e68cc7f9b54d6d4d8d63b7
qqbuby <qqbuby@gmail.com>
2018-06-07 17:45:22 +0800

    Update P-CODE of INSERTION_SORT.go

--- INSERTION_SORT.go
+++ INSERTION_SORT.go
@@ -15,14 +15,13 @@ func main() {
 }
 
 // INSERT-SORT(A)
-//
-// for j = 2 to A.length
+//   for j = 2 to A.length
 //     key = A[j]
 //     // Insert A[j] into the sorted sequence A[1..j-1].
 //     i = j - 1
 //     while i > 0 and A[i] > key
-//         A[i+1] = A[i]
-//         i = i - 1
+//       A[i+1] = A[i]
+//       i = i - 1
 //     A[i+1] = key
 func INSERTION_SORT(A []int) {
 	j := 1
