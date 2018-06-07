8c3aa93cbd1bea253fbab8e949617b56f355c80a
qqbuby <qqbuby@gmail.com>
2018-06-07 17:47:39 +0800

    Update P-CODE of BUBBLE_SORT.go

--- BUBBLE_SORT.go
+++ BUBBLE_SORT.go
@@ -15,11 +15,10 @@ func main() {
 }
 
 // BUBBLE_SORT(A)
-//
-// for i = 1 to A.length
+//   for i = 1 to A.length
 //     for j = A.length downto i + 1
-//        if A[j] < A[j-1]
-//             exchange A[j] with A[j-1]
+//       if A[j] < A[j-1]
+//         exchange A[j] with A[j-1]
 func BUBBLE_SORT(A []int) {
 	for i := 0; i < len(A); i++ {
 		for j := len(A) - 1; j > i; j-- {
